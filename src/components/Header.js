65a1bcb206aebd48d79a4b81a481966be3bf46f1
dylanmarc <diloubilou@gmail.com>
2020-03-13 13:22:24 +1300

    first commit, react header excersise

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div id='header'>
+            <img src='https://i.pinimg.com/originals/b1/5a/ad/b15aadbcb018a585fe5890c2344f13ff.png' />
+            <div className='pageLinks'>
+                <a href=''>Page 1</a>
+                <a href=''>Page 2</a>
+                <a href=''>Page 3</a>
+            </div>
+                
+        </div>
+    )
+}
