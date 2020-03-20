d0292946de131e4a45eaaefdbe6ff9c891baa6f7
dylanmarc <diloubilou@gmail.com>
2020-03-20 13:20:23 +1300

    created and implemented contact card component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import Header from './components/Header.js';
+import ContactCard from './components/ContactCard.js';
 import logo from './logo.svg';
 import './App.css';
+import data from './data/employee.json';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <header className="App-header">
+        <div className='employees'>
+      {data.Employees.map(i => <ContactCard firstName={i.firstName} lastName={i.lastName} jobTitle={i.jobTitle} />)}
+        </div>
         
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
